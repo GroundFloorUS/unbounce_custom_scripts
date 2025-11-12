450409bd194e0278ed67f4cff167310a2bcf0fd4
Tyler Walker <tyler.walker@gmail.com>
2025-11-12 11:04:59 -0500

    update pr test env

--- get-referral-session-id-pr.js
+++ get-referral-session-id-pr.js
@@ -1,9 +1,7 @@
 import { establishReferralSession } from './establish-referral-session.js';
 
 // These variables should be updated to point at whatever lower environment you are testing.
-// If you are using a PR environment, the token will look like this (given a Jira ID of GF-123):
-// `const apiConsumerToken = "Token token=gf-123-secret";`
-const graphQLEndpoint = "https://api.staging.gandalf-struts.com/graphql_api/v1";
-const apiConsumerToken = "Token token=sk_mnvJHs83KKlkjasdiFxxilak";
+const graphQLEndpoint = "https://api.sc-1038.gandalf-struts.com/graphql_api/v1";
+const apiConsumerToken = "Token token=sc-1038-secret";
 
 establishReferralSession(graphQLEndpoint, apiConsumerToken);
