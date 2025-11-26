9a03c95beaa5fc772ef76cab8dc7c48a80ed34b9
onebigdoor <tyler.walker@gmail.com>
2025-11-26 11:22:05 -0500

    Update API consumer token for testing environment

--- get-referral-session-id-pr.js
+++ get-referral-session-id-pr.js
@@ -2,6 +2,6 @@ import { establishReferralSession } from './establish-referral-session.js';
 
 // These variables should be updated to point at whatever lower environment you are testing.
 const graphQLEndpoint = "https://api.sc-1100.gandalf-struts.com/graphql_api/v1";
-const apiConsumerToken = "Token token=sc-sc-1100-secret";
+const apiConsumerToken = "Token token=sc-1100-secret";
 
 establishReferralSession(graphQLEndpoint, apiConsumerToken);
