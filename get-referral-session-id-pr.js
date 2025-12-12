083fd0eb266061d89bccc9be792e7f180413042c
onebigdoor <tyler.walker@gmail.com>
2025-12-12 15:33:30 -0500

    Update GraphQL endpoint and token for testing

--- get-referral-session-id-pr.js
+++ get-referral-session-id-pr.js
@@ -1,7 +1,7 @@
 import { establishReferralSession } from './establish-referral-session.js';
 
 // These variables should be updated to point at whatever lower environment you are testing.
-const graphQLEndpoint = "https://api.sc-1146.gandalf-struts.com/graphql_api/v1";
-const apiConsumerToken = "Token token=sc-1146-secret";
+const graphQLEndpoint = "https://api.sc-1147.gandalf-struts.com/graphql_api/v1";
+const apiConsumerToken = "Token token=sc-1147-secret";
 
 establishReferralSession(graphQLEndpoint, apiConsumerToken);
